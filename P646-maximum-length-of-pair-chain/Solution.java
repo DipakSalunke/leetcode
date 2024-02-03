0e8d25abec3770f87482fdddeffde4a9ab4a0117
Dipak Salunke <dipakbs810@gmail.com>
2024-02-04 00:18:17 +0530

    P646

--- P646-maximum-length-of-pair-chain/Solution.java
+++ P646-maximum-length-of-pair-chain/Solution.java
@@ -1,7 +1,5 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Objects;
 
 class Solution {
     public static int findLongestChain(int[][] pairs) {
@@ -10,14 +8,7 @@ class Solution {
         for (int i = 0; i < pairs.length; i++) {
             pairArr[i] = new Pair(pairs[i][0], pairs[i][1]);
         }
-        Arrays.sort(pairArr, (pair1, pair2) -> {
-            Integer abs = Math.abs(pair1.end - pair1.start);
-            Integer abs2 = Math.abs(pair2.end- pair2.start);
-            return abs.equals(abs2)
-                    ? pair1.start.compareTo(pair2.start)
-                    : abs.compareTo(abs2);
-        });
-        System.out.println(Arrays.toString(pairArr));
+        Arrays.sort(pairArr, Comparator.comparing(pair -> pair.end));
         int count = 1;
         int currentEnd = pairArr[0].end;
         for (int i = 0; i < pairArr.length; i++) {
@@ -29,25 +20,15 @@ class Solution {
         return count;
     }
 
-    static class Pair {
-        Integer start, end = 0;
+    public static void main(String[] args) {
+        System.out.println(findLongestChain(new int[][]{{-6, 9}, {1, 6}, {8, 10}, {-1, 4}, {-6, -2}, {-9, 8}, {-5, 3}, {0, 3}}));
+    }
 
+    static class Pair {
+        Integer start, end;
         Pair(int s, int e) {
             start = s;
             end = e;
         }
-
-        @Override
-        public String toString() {
-            return "Pair{" +
-                    "start=" + start +
-                    ", end=" + end +
-                    '}' + '\n';
-        }
-    }
-
-    public static void main(String[] args) {
-        int a = findLongestChain(new int[][]{{-6,9},{1,6},{8,10},{-1,4},{-6,-2},{-9,8},{-5,3},{0,3}});
-        System.out.println(a);
     }
 }
